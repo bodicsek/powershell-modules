030f91ae757b9d0d5746b9463de8278402da4d53
David Nabraczky <david.nabraczky@gmail.com>
2016-01-24 18:22:37 +0100

    TodoTxt: parsing a file

---
+++ TodoTxt/ParseFile.Tests.ps1
@@ -0,0 +1,44 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Import-TodoTxtFile" {
+
+    Context "when the given file does not exist" {
+
+        $nonexistentPath = ".\todo.txt"
+
+        It "should throw" {
+            Import-TodoTxtFile $nonexistentPath | Should Throw
+        }
+    }
+
+    Context "when the given file is empty" {
+        In $TestDrive {
+
+            $emptyFile = ".\todo.txt"
+            Set-Content $emptyFile -Value ""
+
+            It "returns $null" {
+                $result = Import-TodoTxtFile $emptyFile
+                $result | Should BeNullOrEmpty
+            }
+        }
+    }
+
+    Context "when the given file contains todo.txt lines" {
+        In $TestDrive {
+
+            $todoTxtFile = ".\todo.txt"
+            Set-Content $todoTxtFile -Value "(A) todo1 @home +hobby"
+            Add-Content $todoTxtFile -Value "(B) 2016-01-01 todo2 @office +projectA"
+
+            It "returns one object per line" {
+                $todos = Import-TodoTxtFile $todoTxtFile
+
+                ($todos | measure | select -ExpandProperty Count) |
+                    Should Be 2
+            }
+        }
+    }
+}
---
+++ TodoTxt/ParseFile.ps1
@@ -0,0 +1,43 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+. "$here\ParseLine.ps1"
+
+<#
+.Synopsis
+    Imports a todo.txt file
+.DESCRIPTION
+    The given file is parsed as a todo.txt file using the Import-TodoTxtLine cmdlet.
+    The return value is $null if there is no content in the file.
+    The return value is an array of parsed custom objects (see: Get-Hel Import-TodoTxtLine) if there were valid todo.txt lines in the file.
+    A 'File not found' exception is thrown if the file is nonexistent.
+.EXAMPLE
+    Import-TodoTxtFile todo.txt
+.EXAMPLE
+    Import-TodoTxtFile -File todo.txt
+#>
+function Import-TodoTxtFile
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Full path of a todo.txt file.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $File
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $lines = Get-Content $File | where Length -gt 0
+        foreach ($line in $lines)
+        {
+            Import-TodoTxtLine $line
+        }
+    }
+    End
+    {
+    }
+}
